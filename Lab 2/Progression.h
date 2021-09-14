6256436b692a870c56d0cecb85a93ba2c0ce3bc3
Elias Magdaleno <eliasrmagdaleno@gmail.com>
2021-09-14 12:37:15 -0700

    lab 2

---
+++ Lab 2/Progression.h
@@ -0,0 +1,38 @@
+class Progression
+{ // a generic progression
+public:
+    Progression(long f = 0) // constructor
+        : first(f), cur(f)
+    {
+    }
+    virtual int ˜Progression(){};     // destructor
+    void printProgression(int n); // print the first n values
+protected:
+    virtual long firstValue(); // reset
+    virtual long nextValue();  // advance
+protected:
+    long first; // first value
+    long cur;   // current value
+};
+
+class absoluteProgression: public Progression
+{
+    public:
+        absoluteProgression()
+        {
+        first = 2;
+        second = 200;
+        }
+        absoluteProgression()
+        {
+
+        }
+
+        cur = std::abs (first - second); 
+    protected:
+        long second;
+
+    
+
+};
+
---
+++ Lab 2/absolute.h
@@ -0,0 +1,21 @@
+#include "Progression.h"
+
+class absolute: public Progression
+{
+    public:
+        absolute()
+        {
+        first = 2;
+        second = 200;
+        }
+        absolute()
+        {
+
+        }
+    private:
+        long second;
+
+    
+    Progression::
+};
+
